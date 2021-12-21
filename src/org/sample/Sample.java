7574cafce1f6734fe1d34d1440b5590ad48c866b
tharik9789 <tharik9789@gmail.com>
2021-12-21 12:44:56 +0530

    done

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,9 +2,16 @@ package org.sample;
 
 public class Sample {
 	
+	private void add2() {
+		System.out.println("add2");
+
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Done");
+		
+		
 		
 	}
 
